3690575f33b8dc6a2ca6021af47ff1843b9d88a5
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2021-08-25 01:05:10 +0530

    initial methods and classes

--- matrix_to_uppper_triangular.py
+++ matrix_to_uppper_triangular.py
@@ -1,1 +1,11 @@
+import numpy as np
+
+class Gaussian:
+    def __init__(self, input_matrix):
+        self.matrix = input_matrix
+
+
+class IsSingular(Exception):
+    raise "Matrix is Singular"
+
 
